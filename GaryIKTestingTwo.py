27adfd38aedf8c13bb68d18717be71866bdf7c85
G-Farrell <66701124+G-Farrell@users.noreply.github.com>
2025-03-19 20:28:39 -0400

    small updates

--- GaryIKTestingTwo.py
+++ GaryIKTestingTwo.py
@@ -10,10 +10,12 @@ from ikpy.link import URDFLink
 
 
 
-Base = DHLink(name="Base", d=10, a=0, alpha=-1.57079633, theta=0, bounds = (0, 3.1415926535))
+Base = DHLink(name="Base", d=10, a=0, alpha=-1.57079633, theta=1.57079633, bounds = (0, 3.1415926535))
 Shoulder = DHLink(name="Shoulder", d=0, a=48, alpha=0, theta=-1.57079633, bounds=(0, 3.1415926535))
 Elbow = DHLink(name="Elbow", d=0, a=47, alpha=0, theta=0, bounds=(0, 3.1415926535))
 End = DHLink(name="End", d=0, a=80, alpha=0, theta=0, bounds=(0, 3.1415926535))
 
 robot_arm = Chain(name = "Robot", links = [Base, Shoulder, Elbow, End])
-print(robot_arm.forward_kinematics([0, 0, 0, 0])[:3, 3])
+print(robot_arm.forward_kinematics([0, 1.57, 0, 0])[:3, 3])
+print(180/3.14*robot_arm.inverse_kinematics((0, 0, 160)))
+
