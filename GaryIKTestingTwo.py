6338ce2cbf41b159151b48b1b62f95b97c3e4008
G-Farrell <66701124+G-Farrell@users.noreply.github.com>
2025-03-22 14:26:16 -0400

    Updates to IK testing

--- GaryIKTestingTwo.py
+++ GaryIKTestingTwo.py
@@ -10,12 +10,18 @@ from ikpy.link import URDFLink
 
 
 
-Base = DHLink(name="Base", d=10, a=0, alpha=-1.57079633, theta=1.57079633, bounds = (0, 3.1415926535))
-Shoulder = DHLink(name="Shoulder", d=0, a=48, alpha=0, theta=-1.57079633, bounds=(0, 3.1415926535))
-Elbow = DHLink(name="Elbow", d=0, a=47, alpha=0, theta=0, bounds=(0, 3.1415926535))
-End = DHLink(name="End", d=0, a=80, alpha=0, theta=0, bounds=(0, 3.1415926535))
+Base = DHLink(name="Base", d=0, a=0, alpha=-1.57079633, theta=1.57079633, bounds = (-1.57079633, 1.57079633))
+Shoulder = DHLink(name="Shoulder", d=0, a=48, alpha=0, theta=4.71238899, bounds=(-1.57079633, 1.57079633))
+Elbow = DHLink(name="Elbow", d=0, a=47, alpha=0, theta=0, bounds=(-1.57079633, 1.57079633))
+End = DHLink(name="End", d=0, a=80, alpha=0, theta=1.57079633, bounds=(-1.57079633, 1.57079633))
 
 robot_arm = Chain(name = "Robot", links = [Base, Shoulder, Elbow, End])
-print(robot_arm.forward_kinematics([0, 1.57, 0, 0])[:3, 3])
-print(180/3.14*robot_arm.inverse_kinematics((0, 0, 160)))
+#print(robot_arm.forward_kinematics([0, 1.57, 0, 0])[:3, 3])
 
+format_float = np.vectorize(lambda x: "%.6f" % x)
+radVals = format_float(robot_arm.inverse_kinematics((0, 75, 75))) #Get rad values out of scientific notation
+radVals = np.array(radVals, dtype=float) #Turn this into an np array
+degVals = 57.2957795*radVals #Turn rad to deg
+degVals = degVals+90 #Add 90 to make up for the 90 removed in the DH paramaters 
+degVals = np.round(degVals).astype(int) #Cast to int
+print(degVals)
--- arduinoPythonTest.py
+++ arduinoPythonTest.py
@@ -12,6 +12,7 @@ serialInst.baudrate = 115200
 serialInst.port = "/dev/cu.usbserial-1120"
 serialInst.open()
 
+
 while True:
     command = input() + "\n"
     serialInst.write(command.encode('utf-8'))
