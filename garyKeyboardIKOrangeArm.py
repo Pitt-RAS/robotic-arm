070e92944da97025482d7b87207d85341ad1bbf8
G-Farrell <66701124+G-Farrell@users.noreply.github.com>
2025-04-05 15:25:10 -0400

    keyboard IK

--- garyKeyboardIKOrangeArm.py
+++ garyKeyboardIKOrangeArm.py
@@ -10,6 +10,10 @@ portsList = []
 
 end = False
 
+x = 0
+y = 80
+z = 80
+
 for one in ports:
     portsList.append(str(one))
     print(str(one))
@@ -17,7 +21,6 @@ for one in ports:
 serialInst.baudrate = 115200
 serialInst.port = "/dev/cu.usbserial-1120"
 serialInst.open()
-
 
 Base = DHLink(name="Base", d=0, a=0, alpha=-1.57079633, theta=1.57079633, bounds = (-1.57079633, 1.57079633))
 Shoulder = DHLink(name="Shoulder", d=0, a=48, alpha=0, theta=4.71238899, bounds=(-1.57079633, 1.57079633))
@@ -35,46 +38,41 @@ Elbow.bounds = (-1.57079633, 1.57079633)
 End.name = "End"
 End.bounds = (-1.57079633, 1.57079633)
 
-x = 0
-y = 75
-z = 75
-
-format_float = np.vectorize(lambda x: "%.6f" % x)
-radVals = format_float(robot_arm.inverse_kinematics((x, y, z))) #Get rad values out of scientific notation
-print("Rad vals ", radVals)
-radVals = np.array(radVals, dtype=float) #Turn this into an np array
-degVals = 57.2957795*radVals #Turn rad to deg
-degVals = degVals+90 #Add 90 to make up for the 90 removed in the DH paramaters 
-degVals = np.round(degVals).astype(int) #Cast to int
-print(degVals)
-
-#print("Proceed? y/n")
-
-#response = input()
-
-#if response == "y":
-#    for i in range(len(degVals)):
-#        current = str(i) + str(degVals[i])
-#        command = current + "\n"
-#        serialInst.write(command.encode('utf-8'))
-
 def on_key_release(key):
-    if key == 'q':
-        end = True
-    elif key == 'a':
-        x -= 1
-    elif key == 'd':
-        x += 1
-    elif key == 'w':
-        y += 1
-    elif key == 's':
-        y -= 1
-    elif key == keyboard.Key.up:
-        z += 1
-    elif key == keyboard.Key.down:
-        z -= 1
+    global x, y, z
+    try:
+        if key.char == 'q':
+            end = True
+        elif key.char == 'a':
+            x -= 10
+        elif key.char == 'd':
+            x += 10
+        elif key.char == 'w':
+            y += 10
+        elif key.char == 's':
+            y -= 10
+            if y < 0:
+                y = 0
+    except AttributeError:
+        if key == keyboard.Key.up:
+            z += 10
+        elif key == keyboard.Key.down:
+            z -= 10
+            if z < 0:
+                z = 0
+    
+    format_float = np.vectorize(lambda x: "%.6f" % x)
+    radVals = format_float(robot_arm.inverse_kinematics((x, y, z))) #Get rad values out of scientific notation
+    #print("Rad vals ", radVals)
+    radVals = np.array(radVals, dtype=float) #Turn this into an np array
+    degVals = 57.2957795*radVals #Turn rad to deg
+    degVals = degVals+90 #Add 90 to make up for the 90 removed in the DH paramaters 
+    degVals = np.round(degVals).astype(int) #Cast to int
     print(x, y, z)
-
+    for i in range(len(degVals)):
+        current = str(i) + str(degVals[i])
+        command = current + "\n"
+        serialInst.write(command.encode('utf-8'))
 
 with keyboard.Listener(on_release=on_key_release) as listener:
     listener.join()
